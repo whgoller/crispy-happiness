15a6600752405e6f6ccc5e9c4f5e2bbf757278ba
Wendy Goller <whgoller@gmail.com>
2021-06-18 18:12:24 -0500

    part 1 of roboFriends app made during class

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -1,28 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
- 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+import React, { Component } from "react";
+import CardArray from './CardArray';
+import SearchBox from './SearchBox';
+import { robots } from './robots';
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            robots: robots,
+            searchfield: ""
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })
+
+    }
+
+    render() {
+        const filteredRobots = this.state.robots.filter(robots => {
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        });
+        return (
+            <div className="tc">
+                <h1>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange} />
+                <CardArray robots={filteredRobots} />
+            </div>
+        );
+    }
 }
- 
 export default App;
--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import registerServiceWorker from './registerServiceWorker';
+// import reportWebVitals from './reportWebVitals';
+import 'tachyons';
 
+// Hello is the class and the greating is a property object accessible in the Hello.js file as .prop
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -11,7 +14,9 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+registerServiceWorker();
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
