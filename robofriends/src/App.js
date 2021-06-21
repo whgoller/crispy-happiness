5b2aed7b221b25848d0045e3f3cd9578d950c785
Wendy Goller <whgoller@gmail.com>
2021-06-21 11:34:13 -0500

    Changes for initial app and adding Sega styles for the header.

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -2,14 +2,18 @@ import React, { Component } from "react";
 import CardArray from './CardArray';
 import SearchBox from './SearchBox';
 import { robots } from './robots';
+import './App.css'
 
 class App extends Component {
     constructor() {
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ""
         }
+    }
+    componentDidMount() {
+        this.setState({ robots: robots });
     }
 
     onSearchChange = (event) => {
@@ -23,7 +27,7 @@ class App extends Component {
         });
         return (
             <div className="tc">
-                <h1>RoboFriends</h1>
+                <h1 className="f1">RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange} />
                 <CardArray robots={filteredRobots} />
             </div>
