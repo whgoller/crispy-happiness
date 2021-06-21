4c4eccdb49e1c7ad9228fd7459931a8474ebbd78
Wendy Goller <whgoller@gmail.com>
2021-06-21 11:50:44 -0500

    Add api call using placeholder json for users

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import CardArray from './CardArray';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
 import './App.css'
 
 class App extends Component {
@@ -13,7 +12,9 @@ class App extends Component {
         }
     }
     componentDidMount() {
-        this.setState({ robots: robots });
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({ robots: users }));
     }
 
     onSearchChange = (event) => {
@@ -25,13 +26,17 @@ class App extends Component {
         const filteredRobots = this.state.robots.filter(robots => {
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         });
-        return (
-            <div className="tc">
-                <h1 className="f1">RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange} />
-                <CardArray robots={filteredRobots} />
-            </div>
-        );
+        if (this.state.robots.length === 0) {
+            return <h1>Loading</h1>
+        } else {
+            return (
+                <div className="tc">
+                    <h1 className="f1">RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <CardArray robots={filteredRobots} />
+                </div>
+            );
+        }
     }
 }
 export default App;
